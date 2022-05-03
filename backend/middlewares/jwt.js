faf7c666d9519c1cad1d9fc7b95ae3022863ccf6
Yaroslavna Zavyalova <YZavyalova2@gmail.com>
2022-05-03 20:52:40 +0500

    sprint 15

--- backend/middlewares/jwt.js
+++ backend/middlewares/jwt.js
@@ -1,17 +1,19 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjcwZmY5ZWNmMzM0NDg5MWZjNWFlOTUiLCJpYXQiOjE2NTE1ODM2NzQsImV4cCI6MTY1MjE4ODQ3NH0.EiRwBFjh1qaKurmvJgeqt_qbsSH6ZsiAfn-6OUJDKJM'; // вставьте сюда JWT, который вернул публичный сервер студента
-const SECRET_KEY_DEV = 'JWT_SECRET'; // вставьте сюда секретный ключ для разработки из кода студента
-try {
-  const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-  console.log('\x1b[31m%s\x1b[0m', `
-  Надо исправить. В продакшне используется тот же
-  секретный ключ, что и в режиме разработки.
-  `);
-} catch (err) {
-  if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-    console.log('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются');
-  } else {
-    console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
-  }
-}
+// const { NODE_ENV, JWT_SECRET } = process.env;
+
+// const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjcwZmY5ZWNmMzM0NDg5MWZjNWFlOTUiLCJpYXQiOjE2NTE1ODM2NzQsImV4cCI6MTY1MjE4ODQ3NH0.EiRwBFjh1qaKurmvJgeqt_qbsSH6ZsiAfn-6OUJDKJM'; // вставьте сюда JWT, который вернул публичный сервер студента
+// const SECRET_KEY_DEV = 'JWT_SECRET'; // вставьте сюда секретный ключ для разработки из кода студента
+// try {
+//   const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+//   console.log('\x1b[31m%s\x1b[0m', `
+//   Надо исправить. В продакшне используется тот же
+//   секретный ключ, что и в режиме разработки.
+//   `);
+// } catch (err) {
+//   if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+//     console.log('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются');
+//   } else {
+//     console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
+//   }
+// }
